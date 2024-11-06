2ac93c4020aec0d75dbfeaeb5f1f2842102e3d50
MatthewAraujo <matthewaraujo20@gmail.com>
2024-11-06 12:19:09 -0300

    feat: extract data from caderno.pdf

--- node.js
+++ node.js
@@ -15,13 +15,20 @@ function extractBlockAndQuestion(pdfPath) {
   const pdfBuffer = fs.readFileSync(pdfPath);
 
   pdfParse(pdfBuffer).then(data => {
-    const block = extractForGabarito(data.text);
-    const questionLines = getGabaritoForPdf(block);
+    const block = extractForCaderno(data.text);
+    const questionLines = getCadernoForPdf(block);
 
     console.log(questionLines);
   }).catch(error => {
     console.error('Erro ao processar o PDF:', error);
   });
+}
+
+function extractForCaderno(text) {
+  const pattern = /QUESTÃO 1[\s\S]*?QUESTÃO 70/g;
+  const match = text.match(pattern);
+
+  return match ? match[0] : "Bloco não encontrado!";
 }
 
 function extractForGabarito(text) {
@@ -30,6 +37,54 @@ function extractForGabarito(text) {
 
   return match ? match[0] : "Bloco não encontrado!";
 }
+
+function getCadernoForPdf(block) {
+  const lines = block.split("\n");
+  const questions = [];
+  let currentQuestion = null;
+  let questionContent = '';
+
+  lines.forEach(line => {
+    line = line.trim();
+
+    if (line === "") return;
+
+    const questionMatch = line.match(/^QUESTÃO (\d+) – (.+)/);
+    if (questionMatch) {
+      if (currentQuestion) {
+        // Atribui o conteúdo da questão e suas alternativas
+        currentQuestion.questionContent = questionContent.trim();
+        questions.push(currentQuestion);
+      }
+
+      currentQuestion = {
+        questionNumber: parseInt(questionMatch[1]),
+        questionContent: '',
+        alternatives: []
+      };
+      questionContent = questionMatch[2];
+    }
+    else if (line.match(/^[A-E]\)/)) {
+      const alternativeMatch = line.match(/^([A-E])\) (.+)/);
+      if (alternativeMatch && currentQuestion) {
+        currentQuestion.alternatives.push({
+          [alternativeMatch[1]]: alternativeMatch[2].trim()
+        });
+      }
+    }
+    else if (currentQuestion) {
+      questionContent += ' ' + line;
+    }
+  });
+
+  if (currentQuestion) {
+    currentQuestion.questionContent = questionContent.trim();
+    questions.push(currentQuestion);
+  }
+
+  return questions;
+}
+
 
 function getGabaritoForPdf(block) {
   const lines = block.split("\n").filter(line => line.trim() !== "" && !line.includes("ttps://fundatec.org.br/portal/concursos/") && !line.includes("Fundatec") && !line.includes("Imprimir"));
@@ -56,5 +111,4 @@ function getGabaritoForPdf(block) {
   return questionLines;
 }
 
-
-extractBlockAndQuestion('./gabarito_2023.pdf');
+extractBlockAndQuestion('./caderno_2023.pdf');
