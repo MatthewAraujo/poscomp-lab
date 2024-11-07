3ccb97ffc6042cc14545a595111133f250d487b9
MatthewAraujo <matthewaraujo20@gmail.com>
2024-11-06 22:38:43 -0300

    refactor: prova js

--- prova.js
+++ prova.js
@@ -1,321 +1,232 @@
-let examData = [];
-let currentQuestionIndex = 0;
-let userResponded = false
+class ExamManager {
+  constructor() {
+    this.examData = [];
+    this.currentQuestionIndex = 0;
+    this.userResponded = false;
+    this.examDate = window.location.href.split("#").at(-1)
+  }
 
-async function getExam(exam) {
-  try {
-    const response = await fetch(`http://localhost/poscomp-lab/assets/provas/poscomp/${exam}.json`, { cache: "no-cache" });
+  async initialize(timerEnabled) {
+    this.examData = await this.fetchExamData(this.examDate);
+    this.renderBasePage(timerEnabled);
+    this.displayQuestion();
+    if (timerEnabled) new Timer(this.finishExam.bind(this)).start();
+  }
 
-    if (!response.ok) {
-      throw new Error(`Erro ao buscar dados: ${response.status}`);
+  async fetchExamData(exam) {
+    try {
+      const response = await fetch(`http://localhost/poscomp-lab/assets/provas/poscomp/${exam}.json`, { cache: "no-cache" });
+      if (!response.ok) throw new Error(`Erro ao buscar dados: ${response.status}`);
+      return await response.json();
+    } catch (error) {
+      console.error("Erro ao obter exame:", error);
+      return [];
     }
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error("Erro ao obter exame:", error);
-  }
-}
-
-function setupPage(timer) {
-  const appDiv = document.getElementById("app");
-  appDiv.innerHTML = "";
-
-  const mainContainer = document.createElement("div");
-
-  const title = document.createElement("h1");
-  title.className = "text-2xl font-bold mb-4";
-  title.textContent = "Questões do Exame";
-  mainContainer.appendChild(title);
-
-  const questionsDiv = document.createElement("div");
-  questionsDiv.id = "questions";
-  questionsDiv.className = "space-y-6";
-  mainContainer.appendChild(questionsDiv);
-
-  const navDiv = document.createElement("div");
-  navDiv.id = "navigation";
-  navDiv.className = "flex justify-between mt-6";
-  mainContainer.appendChild(navDiv);
-
-  appDiv.appendChild(mainContainer);
-
-  if (timer) {
-    const timerDiv = document.createElement("div");
-    timerDiv.id = "timer";
-    timerDiv.className = "absolute top-6 right-6 text-xl font-bold bg-black text-white px-4 py-2 rounded-lg shadow-md";
-    appDiv.appendChild(timerDiv);
   }
 
-  renderNavigationButtons();
-}
+  renderBasePage(timerEnabled) {
+    const appDiv = document.getElementById("app");
+    appDiv.innerHTML = "";
 
-function renderNavigationButtons() {
-  const navDiv = document.getElementById("navigation");
+    const mainContainer = document.createElement("div");
+    mainContainer.innerHTML = `
+      <h1 class="text-2xl font-bold mb-4">Questões do Exame</h1>
+      <div id="questions" class="space-y-6"></div>
+      <div id="navigation" class="flex justify-between mt-6"></div>
+    `;
+    appDiv.appendChild(mainContainer);
 
-  navDiv.innerHTML = "";
-
-  if (currentQuestionIndex > 0) {
-    const prevButton = document.createElement("button");
-    prevButton.className = "bg-gray-300 text-gray-700 py-2 px-4 rounded-lg hover:bg-gray-400";
-    prevButton.textContent = "← Anterior";
-    prevButton.onclick = () => changeQuestion(-1);
-    navDiv.appendChild(prevButton);
-  }
-
-  if (currentQuestionIndex < examData.length - 1) {
-    const nextButton = document.createElement("button");
-    nextButton.className = "bg-gray-300 text-gray-700 py-2 px-4 rounded-lg hover:bg-gray-400";
-    nextButton.textContent = "Próxima →";
-    nextButton.onclick = () => changeQuestion(1);
-    navDiv.appendChild(nextButton);
-  }
-}
-
-function displayQuestion(index) {
-  const questionsDiv = document.getElementById("questions");
-  questionsDiv.innerHTML = "";
-
-  const question = examData[index];
-  if (question) insertQuestion(question);
-}
-
-function changeQuestion(direction) {
-  console.log("Direction: ", direction);
-  currentQuestionIndex += direction;
-
-  if (currentQuestionIndex < 0) currentQuestionIndex = 0;
-  if (currentQuestionIndex >= examData.length) currentQuestionIndex = examData.length - 1;
-
-  displayQuestion(currentQuestionIndex);
-  renderNavigationButtons();
-}
-
-
-function insertQuestion(question) {
-  const questionAlreadyAsked = localStorage.getItem(`answer-${question.id}`)
-  const questionsDiv = document.getElementById("questions");
-
-  const questionContainer = document.createElement("div");
-  questionContainer.className = "p-4 bg-gray-50 rounded-lg shadow-md";
-
-  const questionText = document.createElement("p");
-  questionText.className = "text-lg font-semibold text-gray-800 mb-3";
-  questionText.textContent = `${question.id}. ${question.content}`;
-  questionContainer.appendChild(questionText);
-
-  const alternativesList = document.createElement("ul");
-  alternativesList.className = "space-y-2";
-
-  for (const alternative of question.alternatives) {
-    const [key, value] = Object.entries(alternative)[0];
-
-    const alternativeItem = document.createElement("li");
-
-    const label = document.createElement("label");
-    label.className = "flex items-center space-x-2";
-
-    const input = document.createElement("input");
-    input.type = "radio";
-    input.name = `question-${question.questionId}`;
-    input.value = key;
-    input.className = "form-radio text-blue-500";
-    if (questionAlreadyAsked && key === questionAlreadyAsked) {
-      input.defaultChecked = true;
+    if (timerEnabled) {
+      const timerDiv = document.createElement("div");
+      timerDiv.id = "timer";
+      timerDiv.className = "absolute top-6 right-6 text-xl font-bold bg-black text-white px-4 py-2 rounded-lg shadow-md";
+      appDiv.appendChild(timerDiv);
     }
 
-    const text = document.createElement("span");
-    text.textContent = `${key.toUpperCase()}: ${value}`;
+    this.renderNavigationButtons();
+  }
 
-    label.appendChild(input);
-    label.appendChild(text);
-    alternativeItem.appendChild(label);
-    alternativesList.appendChild(alternativeItem);
+  renderNavigationButtons() {
+    const navDiv = document.getElementById("navigation");
+    navDiv.innerHTML = "";
+
+    if (this.currentQuestionIndex > 0) {
+      const prevButton = this.createNavButton("← Anterior", () => this.changeQuestion(-1));
+      navDiv.appendChild(prevButton);
+    }
+
+    if (this.currentQuestionIndex < this.examData.length - 1) {
+      const nextButton = this.createNavButton("Próxima →", () => this.changeQuestion(1));
+      navDiv.appendChild(nextButton);
+    }
+
+    this.renderFinishButton(navDiv);
+  }
+
+  createNavButton(text, onClick) {
+    const button = document.createElement("button");
+    button.className = "bg-gray-300 text-gray-700 py-2 px-4 rounded-lg hover:bg-gray-400";
+    button.textContent = text;
+    button.onclick = onClick;
+    return button;
+  }
+
+  renderFinishButton(navDiv) {
+    const finishButton = document.getElementById("finishButton");
+    if (finishButton) finishButton.remove();
+
+    if (this.currentQuestionIndex === this.examData.length - 1 && this.userRespondedAllQuestions()) {
+      const finishButton = document.createElement("button");
+      finishButton.id = "finishButton";
+      finishButton.className = "bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 transition duration-300";
+      finishButton.textContent = "Finalizar Prova";
+      finishButton.onclick = this.finishExam.bind(this);
+      navDiv.appendChild(finishButton);
+    }
+  }
+
+  displayQuestion() {
+    const questionsDiv = document.getElementById("questions");
+    questionsDiv.innerHTML = "";
+    new QuestionRenderer(this.examData[this.currentQuestionIndex], this.submitAnswer.bind(this)).render(questionsDiv);
+  }
+
+  changeQuestion(direction) {
+    this.currentQuestionIndex = Math.min(Math.max(this.currentQuestionIndex + direction, 0), this.examData.length - 1);
+    this.displayQuestion();
+    this.renderNavigationButtons();
+  }
+
+  submitAnswer(questionId, selectedOption) {
+    if (selectedOption) {
+      localStorage.setItem(`answer-${questionId}`, selectedOption);
+      this.userResponded = this.userRespondedAllQuestions();
+      new ToastNotification(questionId, selectedOption).show();
+      this.renderNavigationButtons();
+    } else {
+      console.log("Por favor, selecione uma alternativa antes de enviar.");
+    }
+  }
+
+  userRespondedAllQuestions() {
+    return this.examData.length === Object.keys(localStorage).filter(key => key.startsWith("answer-")).length;
+  }
+
+  finishExam() {
+    const correctAnswers = this.examData.reduce((acc, { id, gabarito }) => {
+      return acc + (localStorage.getItem(`answer-${id}`) === gabarito ? 1 : 0);
+    }, 0);
+
+    this.displayFinishPage(correctAnswers);
+  }
+
+  displayFinishPage(correctAnswers) {
+    const appDiv = document.getElementById("app");
+    appDiv.innerHTML = `
+      <div>
+        <h1 class="text-3xl font-bold mb-4">Simulado Finalizado!</h1>
+        <p class="text-xl mb-6">Você acertou ${correctAnswers} de ${this.examData.length} questões.</p>
+        <button class="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600" onclick="alert('Exibindo resultados detalhados...')">Ver Resultados Detalhados</button>
+        <button class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 ml-4" onclick="localStorage.clear(); window.location.href = 'http://localhost/poscomp-lab/index.html'">Finalizar Simulado</button>
+      </div>
+    `;
+  }
+}
+
+class QuestionRenderer {
+  constructor(question, submitAnswer) {
+    this.question = question;
+    this.submitAnswer = submitAnswer;
+  }
+
+  render(parentDiv) {
+    const questionContainer = document.createElement("div");
+    questionContainer.className = "p-4 bg-gray-50 rounded-lg shadow-md";
+
+    questionContainer.innerHTML = `
+      <p class="text-lg font-semibold text-gray-800 mb-3">${this.question.id}. ${this.question.content}</p>
+      <ul class="space-y-2">${this.renderAlternatives()}</ul>
+    `;
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.className = "flex justify-between mt-4";
+    const submitButton = document.createElement("button");
+    submitButton.className = "bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600";
+    submitButton.textContent = "Salvar Resposta";
+    submitButton.onclick = () => this.submitAnswer(this.question.id, this.getSelectedOption());
+    buttonContainer.appendChild(submitButton);
+
+    questionContainer.appendChild(buttonContainer);
+    parentDiv.appendChild(questionContainer);
+  }
+
+  renderAlternatives() {
+    return this.question.alternatives.map((alt, index) => {
+      const key = Object.keys(alt)[0];
+      const value = alt[key];
+
+      return `
+      <li class="flex items-center space-x-3">
+        <input type="radio" name="question-${this.question.id}" value="${value}" id="alt-${this.question.id}-${index}" class="form-radio">
+        <label for="alt-${this.question.id}-${index}" class="text-gray-700">${key}: ${value}</label>
+      </li>
+    `;
+    }).join("");
   }
 
 
-  questionContainer.appendChild(alternativesList);
 
-  const buttonContainer = document.createElement("div");
-  buttonContainer.className = "flex justify-between mt-4";
-
-  const submitButton = document.createElement("button");
-  submitButton.className = "bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300";
-  submitButton.textContent = "Enviar";
-  submitButton.onclick = () => submitAnswer(question.id);
-  buttonContainer.appendChild(submitButton);
-
-  questionContainer.appendChild(buttonContainer);
-  questionsDiv.appendChild(questionContainer);
-}
-
-function renderFinishButton() {
-  const navDiv = document.getElementById("navigation");
-
-  const existingFinishButton = document.getElementById("finishButton");
-  if (existingFinishButton) {
-    existingFinishButton.remove();
-  }
-
-  if (currentQuestionIndex === examData.length - 1 && userRespondedAllQuestions()) {
-    const finishButton = document.createElement("button");
-    finishButton.id = "finishButton";
-    finishButton.className = "bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 transition duration-300";
-    finishButton.textContent = "Finalizar Prova";
-    finishButton.onclick = finishExam;
-
-    navDiv.appendChild(finishButton);
+  getSelectedOption() {
+    const selected = document.querySelector(`input[name="question-${this.question.id}"]:checked`);
+    return selected ? selected.value : null;
   }
 }
 
-
-function userRespondedAllQuestions() {
-  return localStorage.length === examData.length;
-}
-
-function submitAnswer(questionId) {
-  const selectedOption = document.querySelector(`input[name="question-${questionId}"]:checked`);
-  if (selectedOption) {
-    saveAnswer(questionId, selectedOption.value.toLocaleLowerCase())
-  } else {
-    console.log("Por favor, selecione uma alternativa antes de enviar.");
+class Timer {
+  constructor(onFinish) {
+    this.onFinish = onFinish;
+    this.timeLeft = 60 * 60; // 1 hour in seconds
   }
-  userResponded = userRespondedAllQuestions()
-  toast({ id: questionId, question: selectedOption.value.toLocaleLowerCase() })
-  renderFinishButton()
-}
 
-
-
-function startTimer() {
-  let hours = 3;
-  let minutes = 59;
-  let seconds = 60;
-
-  const timerDiv = document.getElementById("timer");
-
-  const timer = setInterval(function () {
-    seconds--;
-
-    if (seconds < 0) {
-      minutes--;
-      seconds = 59;
-      if (minutes < 0) {
-        hours--;
-        minutes = 59;
-        if (hours < 0) {
-          clearInterval(timer);
-          finishExam()
-        }
+  start() {
+    this.interval = setInterval(() => {
+      this.timeLeft -= 1;
+      if (this.timeLeft <= 0) {
+        clearInterval(this.interval);
+        this.onFinish();
       }
-    }
+      this.updateTimerDisplay();
+    }, 1000);
+  }
 
-    timerDiv.textContent = `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-  }, 1000);
-}
-
-async function startExam(timer) {
-  const exam_type = window.location.href.split("#").at(-1)
-  examData = await getExam(exam_type)
-  setupPage(timer)
-  displayQuestion(currentQuestionIndex);
-  if (timer) {
-    startTimer()
+  updateTimerDisplay() {
+    const minutes = Math.floor(this.timeLeft / 60);
+    const seconds = this.timeLeft % 60;
+    document.getElementById("timer").textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   }
 }
 
-function finishExam() {
-  const template = examData.map(exam => {
-    return {
-      "gabarito": exam.gabarito,
-      "id": exam.id
-    };
-  });
+class ToastNotification {
+  constructor(questionId, answer) {
+    this.questionId = questionId;
+    this.answer = answer;
+  }
 
-  let correctAnswers = 0;
+  show() {
+    const notification = document.createElement("div");
+    notification.className = "toast fixed bottom-4 right-4 bg-green-500 text-white p-3 rounded-lg shadow-md";
+    notification.textContent = `Resposta para a questão ${this.questionId} salva com sucesso!`;
+    document.body.appendChild(notification);
 
-  const user_alternatives = template.map((answer) => {
-    const userAnswer = localStorage.getItem(`answer-${answer.id}`);
-
-    if (userAnswer && userAnswer === answer.gabarito) {
-      correctAnswers++;
-    }
-
-    return {
-      "id": answer.id,
-      "alternative": userAnswer ? userAnswer : "Nenhuma resposta",
-      "isCorrect": userAnswer === answer.gabarito
-    };
-  });
-
-  displayFinishPage(correctAnswers)
-}
-
-function displayFinishPage(correctAnswers) {
-  const appDiv = document.getElementById("app");
-  appDiv.innerHTML = "";
-
-  const mainContainer = document.createElement("div");
-
-  const title = document.createElement("h1");
-  title.className = "text-3xl font-bold mb-4";
-  title.textContent = "Simulado Finalizado!";
-  mainContainer.appendChild(title);
-
-  const resultText = document.createElement("p");
-  resultText.className = "text-xl mb-6";
-  resultText.textContent = `Você acertou ${correctAnswers} de ${examData.length} questões.`;
-  mainContainer.appendChild(resultText);
-
-  // Botão para ver resultados detalhados (se necessário)
-  const viewResultsButton = document.createElement("button");
-  viewResultsButton.className = "bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600";
-  viewResultsButton.textContent = "Ver Resultados Detalhados";
-  viewResultsButton.onclick = () => {
-    // Aqui você pode adicionar lógica para exibir mais detalhes, por exemplo, resultados por questão
-    alert("Exibindo resultados detalhados...");
-  };
-  mainContainer.appendChild(viewResultsButton);
-
-  // Botão para finalizar o simulado e sair
-  const finishButton = document.createElement("button");
-  finishButton.className = "bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 ml-4";
-  finishButton.textContent = "Finalizar Simulado";
-  finishButton.onclick = () => {
-    localStorage.clear()
-    window.location.href = "http://localhost/poscomp-lab/index.html"
-  };
-  mainContainer.appendChild(finishButton);
-
-  appDiv.appendChild(mainContainer);
-}
-
-
-function saveAnswer(id, alternative) {
-  const answer = `${alternative}`
-  window.localStorage.setItem(`answer-${id}`, answer)
-}
-
-function toast(question) {
-  const toastContainer = document.createElement("div");
-  toastContainer.className = "fixed bottom-6 right-6 bg-green-500 text-white py-3 px-6 rounded-lg shadow-lg flex items-center space-x-3";
-
-  const icon = document.createElement("span");
-  icon.className = "text-xl font-bold";
-  icon.textContent = "✓";
-
-  const message = document.createElement("span");
-  message.textContent = `Você selecionou a questão ${question.id}: ${question.question}`;
-
-  toastContainer.appendChild(icon);
-  toastContainer.appendChild(message);
-
-  document.body.appendChild(toastContainer);
-
-  setTimeout(() => {
-    toastContainer.classList.add("opacity-0");
     setTimeout(() => {
-      toastContainer.remove();
-    }, 300);
-  }, 3000);
+      notification.classList.add("fade-out");
+      setTimeout(() => notification.remove(), 1000);
+    }, 1500);
+  }
+}
+
+// Iniciar o exame com tempo ou sem tempo
+function startExam(withTimer = false) {
+  const examManager = new ExamManager();
+  examManager.initialize(withTimer);
 }
