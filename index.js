9fdf222b63ad7753d4babd1d5166ab2c046dd561
MatthewAraujo <matthewaraujo20@gmail.com>
2024-11-15 10:52:46 -0300

    ok

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function iniciarExame(prova) {
-  window.location.href = `https://matthewaraujo.github.io/poscomp-lab/prova.html#${prova}`
+  window.location.href = `http://localhost/poscomp-lab/prova.html#${prova}`
 }
 
 function checkLocalStorage() {
-  const currentHash = window.location.hash;
+  const currentHash = window.location.hash
   const savedExamDate = window.localStorage.getItem("examDate");
 
-
-  if (currentHash && currentHash.substring(1) !== savedExamDate && localStorage.length > 0) {
+  if (!currentHash && localStorage.length > 0) {
     const userConfirmed = confirm("Deseja continuar na sua prova antiga?");
     if (userConfirmed) {
-      window.location.href = `https://matthewaraujo.github.io/poscomp-lab/.html#${savedExamDate}`;
+
+      window.sessionStorage.setItem("initialize", true)
+      window.location.href = `http://localhost/poscomp-lab/prova.html#${savedExamDate}`;
     }
   }
 }
